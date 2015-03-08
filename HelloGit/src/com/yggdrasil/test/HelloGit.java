616eccd3f6389516f6a7a2d78386a27e00830b2f
Vich <pravichv@hotmail.com>
2015-03-08 07:05:12 +0700

    Before merging

--- HelloGit/src/com/yggdrasil/test/HelloGit.java
+++ HelloGit/src/com/yggdrasil/test/HelloGit.java
@@ -2,6 +2,9 @@ package com.yggdrasil.test;
 
 public class HelloGit {
 
+	// comment
+	// comment
+	// comment
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("..........");
@@ -9,5 +12,4 @@ public class HelloGit {
 		System.out.println("xxxxxxxxxx");
 		System.out.println("oooooooooo");
 	}
-
 }
